0e03d07954d22d854f3a3a25fb4e991ebc330eb9
byWulf <wulf1337@gmail.com>
2018-06-24 18:23:29 +0200

    Bugfixes

--- BywulfRfc14Bundle.php
+++ BywulfRfc14Bundle.php
@@ -2,9 +2,13 @@
 
 namespace Bywulf\Rfc14Bundle;
 
+use Bywulf\Rfc14Bundle\DependencyInjection\ByWulfRfc14Extension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class BywulfRfc14Bundle extends Bundle
 {
-
+    public function getContainerExtension()
+    {
+        return new ByWulfRfc14Extension();
+    }
 }
--- DependencyInjection/ByWulfRfc14Extension.php
+++ DependencyInjection/ByWulfRfc14Extension.php
@@ -12,12 +12,13 @@ class ByWulfRfc14Extension extends Extension
     /**
      * Loads a specific configuration.
      *
-     * @throws \InvalidArgumentException When provided tag is not defined in this extension
+     * @param array $configs
+     * @param ContainerBuilder $container
      * @throws \Exception
      */
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
+        $loader->load('services.yaml');
     }
 }
