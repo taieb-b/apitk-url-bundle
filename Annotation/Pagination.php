e422115a7198e8f99800e325cb4bbdfaf3778ddc
Michael Wolf <michael.wolf@check24.de>
2018-07-03 13:59:39 +0200

    Fix

--- Annotation/Pagination.php
+++ Annotation/Pagination.php
@@ -14,7 +14,7 @@ class Pagination
     /**
      * Maximum entries per page the client can request.
      *
-     * @var int|null
+     * @var integer|null
      */
     public $maxEntries = null;
 }
