f6f61b1de783a3e7a6eec3da5c9b3b95ed68132d
Dominik Peuscher <dominik.peuscher@check24.de>
2018-12-28 19:15:28 +0100

    Add auto-wire functionality to ApiToolkitRepository

--- Repository/ApiToolkitRepository.php
+++ Repository/ApiToolkitRepository.php
@@ -3,8 +3,11 @@ declare(strict_types=1);
 
 namespace Shopping\ApiTKUrlBundle\Repository;
 
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\NonUniqueResultException;
+use LogicException;
 use Shopping\ApiTKUrlBundle\Exception\PaginationException;
 use Shopping\ApiTKUrlBundle\Service\ApiService;
 
@@ -14,6 +17,25 @@ use Shopping\ApiTKUrlBundle\Service\ApiService;
  */
 class ApiToolkitRepository extends EntityRepository
 {
+
+    /**
+     * @param ManagerRegistry $registry
+     * @param string $entityClass The class name of the entity this repository manages
+     */
+    public function __construct(ManagerRegistry $registry, $entityClass)
+    {
+        /** @var EntityManager $manager */
+        $manager = $registry->getManagerForClass($entityClass);
+
+        if ($manager === null) {
+            throw new LogicException(sprintf(
+                'Could not find the entity manager for class "%s". Check your Doctrine configuration to make sure it is configured to load this entity’s metadata.',
+                $entityClass
+            ));
+        }
+
+        parent::__construct($manager, $manager->getClassMetadata($entityClass));
+    }
 
     /**
      * @param ApiService $apiService
